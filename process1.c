0e484330df1405e545dfcd0fb8f3e5b6561ed743
sarahandrew1012 <sarah.andrew1012@gmail.com>
2025-11-06 15:24:46 -0500

    finished part 2

--- process1.c
+++ process1.c
@@ -1,37 +1,88 @@
+/**
+ * @author Sarah Andrew, 101303468
+ * @author Kaylah Mckez, 101307188
+ * 
+ */
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 
 int main() {
     pid_t pid = fork();
     int counter = 0;
     int cycle = 0;
+    key_t key = ftok(".", 'S');
+    key_t key_sem = ftok(".", 'M');
     int status;
+
+    int shmid = shmget(key, sizeof(int) * 2, IPC_CREAT | 0666);
+    int *shm_ptr = (int *)shmat(shmid, NULL, 0);
+    int semid = semget(key_sem, 1, 0666 | IPC_CREAT);
+
+    // shared memory variables
+    int *multiple = &shm_ptr[0];
+    int *shared_vars = &shm_ptr[1];
+    *multiple = 3; 
+    *shared_vars = 0;
+
+
+    // set it to unlocked
+    semctl(semid, 0, SETVAL, 1);
+
+    // process for wait & lock and also signal & unlock
+    struct sembuf lock  = {0, -1, 0};
+    struct sembuf unlock = {0, 1, 0};
+
+    // ensuring it doesn't fail
+    if (shmid == -1) {
+        perror("shmget");
+        exit(1);
+    }
+
+    if (shm_ptr == (int *)-1) {
+        perror("shmat failed");
+        exit(1);
+    }
 
     if (pid < 0) {
         perror("Fork failed.");
         exit(1);
     }
 
+
     if (pid == 0) {
+        // launching process 2
         char *args[] = {"./process2", NULL};
         execvp("./process2", args);
         perror("execvp failed.");
         exit(1);
     } else {
-        while (1) {
-            if (counter % 3 == 0) {
-                printf("Cycle number: %d - %d is a multiple of 3.\n", cycle, counter);
+        while (*shared_vars <= 500) {
+            // protect share memory
+            semop(semid, &lock, 1);
+
+            if (*shared_vars > 100) {
+                if (*shared_vars % *multiple == 0) {
+                printf("Process 1: Cycle number: %d - %d is a multiple of %d.\n", cycle, *shared_vars, *multiple);
             } else {
-                printf("Cycle number: %d\n", cycle);
+                printf("Process 1: Cycle number: %d - Counter: %d\n", cycle, *shared_vars);
             }
-            counter++;
+        }
+            (*shared_vars)++;
             cycle++;
+            semop(semid, &unlock, 1);
             usleep(50000);
         }
+        waitpid(pid, &status, 0);
+        // clean up processes
+        shmdt(shm_ptr);
+        shmctl(shmid, IPC_RMID, 0);
+        semctl(semid, 0, IPC_RMID);
     }
     return 0;
 }
--- process2.c
+++ process2.c
@@ -1,20 +1,50 @@
+/**
+ * @author Sarah Andrew, 101303468
+ * @author Kaylah Mckez, 101307188
+ * 
+ */
 #include <stdio.h>
 #include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 int main() {
-    int counter = 50;
     int cycle = 0;
+    key_t key = ftok(".", 'S');
+    key_t key_sem = ftok(".", 'M');
 
-    for (; ; cycle++) {
-        if (counter % 3 == 0) {
-            printf("Cycle number: %d - %d is a multiple of 3.\n", cycle, counter);
+    int shmid = shmget(key, sizeof(int) * 2, IPC_CREAT | 0666);
+    int *shm_ptr = (int *)shmat(shmid, NULL, 0);
+    int semid = semget(key_sem, 1, 0666);
+
+    // defining shared memory vars.
+    int *multiple = &shm_ptr[0];
+    int *shared_vars = &shm_ptr[1];
+
+    // lock & unlock process
+    struct sembuf lock = {0, -1, 0};
+    struct sembuf unlock = {0, 1, 0};
+
+    while (*shared_vars <= 100) {
+        usleep(7000);
+    }
+
+    for (;*shared_vars <= 500; cycle++) {
+        // lock/protect
+        semop(semid, &lock, 1);
+        
+        // checking to see if a multiple of 3
+        if (*shared_vars % *multiple == 0) {
+            printf("Process 2: Cycle number: %d - %d is a multiple of %d.\n", cycle, *shared_vars, *multiple);
         } else {
-            printf("Cycle number: %d\n", cycle);
+            printf("Process 2:  Cycle number: %d - Counter: %d\n", cycle, *shared_vars);
         }
-        counter--;
+        // to slow down the output & unlock
+        semop(semid, &unlock, 1);
         usleep(50000);
     }
+    shmdt(shm_ptr);
     return 0;
 }
